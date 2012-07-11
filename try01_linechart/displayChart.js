15b06edfb7566028adabe36a70ffa1e6bdfac9f4
CT Arrington <ct@nowhere.com>
2012-07-11 17:06:30 -0400

    read data from file

--- try01_linechart/displayChart.js
+++ try01_linechart/displayChart.js
@@ -1,14 +1,36 @@
-var config = {
-	graphset: [{type: 'line',
-	series: [{values: [5, 10, 15, 0, 10, 5]}]
-	}]
-};
+$(document).ready(function() {
+	
+	var config = {
+		graphset: [
+		{type: 'line',
+		   series: [{values: [5, 12, 18, 0, 10, 5]}]
+		}]
+	};
+	
+	function processData(data)
+	{
+		console.log("data: "+data);
+		var raw_values = data.split(',');
+		var values = [];
+		
+		for (var ctr=0;ctr<raw_values.length;ctr++)
+		{
+			values[ctr] = parseInt(raw_values[ctr], 10);
+		}
+		
+		console.log("values: "+values);
+		
+		config.graphset[0].series[0].values = values;
+		
+		zingchart.render({
+			data : config,
+			width : 460,
+			height : 460,
+			container : 'zingchart'
+		});
+		
+	}
+	
+	$.get('oneline.dat', processData);
 
-window.onload = function() {
-	zingchart.render({
-		data : config,
-		width : 460,
-		height : 460,
-		container : 'zingchart'
-	});
-}
+});
