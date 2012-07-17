284c9d4a5cfc48e27d73ab14e6e4682008bce171
CT Arrington <ct@nowhere.com>
2012-07-17 09:52:02 -0400

    basic date conversion for short. one line at a time.

--- try01_linechart/displayChart.js
+++ try01_linechart/displayChart.js
@@ -1,16 +1,24 @@
 $(document).ready(function() {
 	
-	var NUMERIC_DATA_REGEXP = /^\d+\.{0,1}\d+\s+/;
-	var SHORT_DATE_TIME_REGEXP = /^\d{2}\/\d{2}\/\d{4}\s\d{2}:\d{2}:\d{2}(\.\d+){0,1}\s+/;
-	var LONG_DATE_TIME_REGEXP = /^^[a-zA-Z]{3}\s+\d{2}\s+\d{4}\s\d{2}:\d{2}:\d{2}(\.\d+){0,1}\s+/;
-	var SHORT_DATE_REGEXP = /^\d{2}\/\d{2}\/\d{4}\s+/;
-	var LONG_DATE_REGEXP = /^[a-zA-Z]{3}\s+\d{2}\s+\d{4}\s+/;		
-	var EXPRESSIONS = [NUMERIC_DATA_REGEXP, SHORT_DATE_TIME_REGEXP, LONG_DATE_TIME_REGEXP, SHORT_DATE_REGEXP, LONG_DATE_REGEXP];
+	var NUMERIC_DATA_REGEXP = {regexp: /^\d+\.{0,1}\d+\s+/, convertor: parseFloat};
+	var SHORT_DATE_TIME_REGEXP = {regexp: /^\d{2}\/\d{2}\/\d{4}\s\d{2}:\d{2}:\d{2}(\.\d+){0,1}\s+/, convertor: null};
+	var LONG_DATE_TIME_REGEXP = {regexp: /^^[a-zA-Z]{3}\s+\d{2}\s+\d{4}\s\d{2}:\d{2}:\d{2}(\.\d+){0,1}\s+/, convertor: null};
+	var SHORT_DATE_REGEXP = {regexp: /^\d{2}\/\d{2}\/\d{4}\s+/, convertor: convertShortDate};
+	var LONG_DATE_REGEXP = {regexp: /^[a-zA-Z]{3}\s+\d{2}\s+\d{4}\s+/, convertor: null};	
+	var PARSERS = [NUMERIC_DATA_REGEXP, SHORT_DATE_TIME_REGEXP, LONG_DATE_TIME_REGEXP, SHORT_DATE_REGEXP, LONG_DATE_REGEXP];
 	
 	var configLine = {
 		graphset: [
 		{plot:{exact:true, decimals:4},
 		 type: 'line',
+		 "scale-x":{
+            "max-items":5,
+            "min-value":1339992000000-1000*60*60*24,
+            "step":1000*60,
+            "transform":{
+                "type":"date",
+                "all":"%D, %d %M %Y<br />%h:%i %A"
+            }},
 		 series: [{"line-width":1}]
 		}]
 	};
@@ -30,23 +38,34 @@ $(document).ready(function() {
 	{
   		return !isNaN(parseFloat(n)) && isFinite(n);
 	}
+	
+	function convertShortDate(rawvalue)
+	{
+		return $.datepicker.parseDate('mm/dd/yy', rawvalue).getTime();
+	}
 
 	function parseNextData(line)
 	{		
 		var match = null;
 		
-		for (var ctr=0;ctr<EXPRESSIONS.length;ctr++)
+		for (var ctr=0;ctr<PARSERS.length;ctr++)
 		{
-			var matches = line.match(EXPRESSIONS[ctr]);
+			var matches = line.match(PARSERS[ctr].regexp);
 			
 			if (matches) 
 			{
 				match = matches[0].trim();
-				break;
+				var convertedValue = match;
+				
+				if (PARSERS[ctr].convertor)
+				{
+					convertedValue = PARSERS[ctr].convertor(match);
+				}
+				return {match: match, value: convertedValue};
 			}
 		}
 				
-		return match;		
+		return null;
 	}
 	
 	function parseDataLine(line)
@@ -57,25 +76,25 @@ $(document).ready(function() {
 			line = line.trim() +' ';  // we need a space at the end of the line, as the regexps use the space as part of the match
 			var data = parseNextData(line);
 								
-			if (!data)
+			if (!data || !data.value)
 			{
 				break;
 			}
 			else
 			{
-				line = line.substr(data.length);
-				if (isNumber(data))
+				line = line.substr(data.match.length);
+				if (isNumber(data.value))
 				{
-					data = parseFloat(data);
+					data.value = parseFloat(data.value);
 				}
-				dataForLine.push(data);				
+				dataForLine.push(data.value);				
 			}				
 		}
 		
 		return dataForLine;
 	}
 	
-	function parseOutputFromFreeFlyer(data)
+	function parseOutputFromAnalysis(data)
 	{
 		var DESCRIPTION_REGEXP = /^\S+/;
 		var COLUMN_HEADER_REGEXP = /^\s+[a-zA-Z]+/;
@@ -122,9 +141,9 @@ $(document).ready(function() {
 	}
 	
 	
-	function processData(data)
+	function processData(rawdata)
 	{
-		var parsed = parseOutputFromFreeFlyer(data);
+		var parsed = parseOutputFromAnalysis(rawdata);
 		var data = parsed.data;
 		
 		config.graphset[0].series[0].values = data;
@@ -139,5 +158,8 @@ $(document).ready(function() {
 	}
 	
 	$.get('short_formatted_longdate_vs_decimal.txt', processData);
+	
+	
+	
 
 });
