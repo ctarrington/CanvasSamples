e0835f0baf39ab7afbd2e744da207fb58d111773
CT Arrington <ct@nowhere.com>
2012-09-27 12:27:39 -0400

    initial

--- d3_try01/try01.js
+++ d3_try01/try01.js
@@ -1,10 +1,23 @@
 $(function() {
 
-var ul = d3.select('#circles01').append('ul');
+var data = [{x:12, y:10, value: 40}, {x:35, y:35, value: 100}];
 
-var li = ul.selectAll("li").data([32, 57, 293]);
+var ul = d3.select('div#list').append('ul');
 
-li.enter().append("li").text(function (d) { return 'data: '+d; });
+var li = ul.selectAll("li").data(data);
+
+li.enter().append("li").text(function (d) { return '('+d.x+', '+d.y+') = '+d.value; });
 li.exit().remove();
+
+
+
+var svg = d3.select('div#circles').append('svg');
+svg.attr('width', 200).attr('height', 200);
+svg.selectAll('circle').data(data).enter().append('circle');
+
+d3.selectAll("circle")
+	.attr("r",  function(d) { return Math.sqrt(2*d.value); } )
+	.attr("cy", function(d) { return d.y; } )
+	.attr("cx", function(d) { return d.x; } );
 
 });
