9caca65d5adf1b97b974037a05ba7d9b7d7d78f8
CT Arrington <ct@nowhere.com>
2012-10-29 18:13:15 -0400

    drag and drop with ghosting calculations, but not much for effects

--- hammer_dnd/hammer_dnd.js
+++ hammer_dnd/hammer_dnd.js
@@ -2,14 +2,66 @@ $(function() {
 	
 var currentDrag = null;
 
-function writeDebugInfo(event)
+function writeDebugInfo(msg)
 {
-	var buffer = event+' id= '+currentDrag.dragged.id+' originalPosition = ('+currentDrag.originalPosition.x+', '+currentDrag.originalPosition.y+') currentPosition = ('+currentDrag.currentPosition.x+', '+currentDrag.currentPosition.y+')';
+	var buffer = ''+msg+' id= '+currentDrag.dragged.id+' originalOffset = ('+currentDrag.originalOffset.left+', '+currentDrag.originalOffset.top+') currentOffset = ('+currentDrag.currentOffset.left+', '+currentDrag.currentOffset.top+')';
+	
+	if (currentDrag.originalDropZone != null)
+	{
+		buffer = buffer + '<br/>currentDrag.originalDropZone = ' +currentDrag.originalDropZone.id; 
+	}
+	
+	if (currentDrag.currentDropZone != null)
+	{
+		buffer = buffer + '<br/>currentDrag.currentDropZone = ' +currentDrag.currentDropZone.id;
+	}
+	
+	$('.dropZone').each( function(index, zone) {
+		var zoneLeft = $(zone).offset().left;
+		var zoneTop = $(zone).offset().top;
+		
+		buffer = buffer + '<br/>Drop Zone '+zone.id+ ' (' +zoneLeft+ ', ' +zoneTop+ ')';
+	});
+		
 	$('div#results').html(buffer);
+}
+
+function findDropZone()
+{
+	if (currentDrag == null) { return null; }
+	
+	var currentLeft = currentDrag.currentOffset.left;
+	var currentTop = currentDrag.currentOffset.top; 
+	
+	var ghostWidth = $('div.ghost').first().width();
+	var ghostHeight = $('div.ghost').first().height();
+	
+	var currentRight = currentLeft+ghostWidth;
+	var currentBottom = currentTop+ghostHeight;
+	
+	var match = null;
+	$('.dropZone').each( function(index, zone) {		
+		var zoneWidth = $(zone).width();
+		var zoneHeight = $(zone).height();
+		var zoneLeft = $(zone).offset().left;
+		var zoneTop = $(zone).offset().top;
+				
+		var topLeftInZone = (currentLeft >= zoneLeft && currentLeft <= (zoneLeft+zoneWidth) && currentTop >= zoneTop && currentTop <= (zoneTop+zoneHeight) );
+		var bottomRightInZone = (currentRight >= zoneLeft && currentRight <= (zoneLeft+zoneWidth) && currentBottom >= zoneTop && currentBottom <= (zoneTop+zoneHeight) ); 
+		if (topLeftInZone && bottomRightInZone)
+		{
+			match = zone;
+		}
+	}); 
+	
+	return match;
 }
 
 function createGhost()
 {
+	var ghostId = currentDrag.dragged.id+'Ghost';
+	$('div#container').append("<div id='"+ghostId+"' class='box ghost'> Ghost </div>");
+	currentDrag.ghostId = ghostId;
 	
 }
 	
@@ -19,7 +71,10 @@ function doDragStart(evt)
 	
 	if ( $(srcElement).hasClass('box') && currentDrag == null )
 	{
-		currentDrag = { dragged: evt.originalEvent.srcElement, originalPosition: {x: srcElement.offsetLeft, y: srcElement.offsetTop}, currentPosition: {x: srcElement.offsetLeft, y: srcElement.offsetTop} };
+		evt.originalEvent.preventDefault();
+		currentDrag = { dragged: evt.originalEvent.srcElement, originalOffset: {left: $(srcElement).offset().left, top: $(srcElement).offset().top}, currentOffset: {left: $(srcElement).offset().left, top: $(srcElement).offset().top} };
+		currentDrag.originalDropZone = findDropZone();
+		currentDrag.currentDropZone = currentDrag.originalDropZone;
 		createGhost();
 		writeDebugInfo('start');
 	}
@@ -32,16 +87,29 @@ function doDrag(evt)
 	
 	if ($(srcElement).hasClass('box') && srcElement === currentDrag.dragged)
 	{
-		currentDrag.currentPosition.x = currentDrag.originalPosition.x + evt.distanceX;
-		currentDrag.currentPosition.y = currentDrag.originalPosition.y + evt.distanceY;
+		evt.originalEvent.preventDefault();
+		currentDrag.currentOffset.left = currentDrag.originalOffset.left + evt.distanceX;
+		currentDrag.currentOffset.top = currentDrag.originalOffset.top + evt.distanceY;
+		currentDrag.currentDropZone = findDropZone();
+		
+		var ghost = $('div.ghost');
+		ghost.offset({ top: currentDrag.currentOffset.top, left: currentDrag.currentOffset.left})
 		writeDebugInfo('drag');
 	}
 }
 
 function doDragEnd(evt)
 {
-	writeDebugInfo('end');
-	currentDrag = null;
+	var srcElement = evt.originalEvent.srcElement;	
+	
+	
+	if (currentDrag != null)
+	{
+		evt.originalEvent.preventDefault();
+		writeDebugInfo('end');
+		$('div.ghost').remove();		
+		currentDrag = null;
+	}
 }
 	
 var container = $('div#container')[0];
